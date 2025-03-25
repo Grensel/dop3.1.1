dd44200a99a0fb62da784c76909a4be971b68141
Grensel <sergeiodinochenko@mail.ru>
2025-03-25 23:43:07 +0300

    feat: completed task

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,30 +1,36 @@
-import {v1} from 'uuid';
-import {FilterValuesType, TodolistType} from "../App";
+import { v1 } from "uuid";
+import { TodolistType } from "../App";
 
+export type AddTodolistActionType = ReturnType<typeof addTodolistAC>;
 
-export type AddTodolistActionType = ReturnType<typeof addTodolistAC>
+type ActionsType = AddTodolistActionType;
 
+const addTodolistACTitle = "ADD-TODOLIST";
 
+const initialState: Array<TodolistType> = [];
 
-
-type ActionsType = AddTodolistActionType
-
-const initialState: Array<TodolistType> =  []
-
-export const todolistsReducer = (state: Array<TodolistType> = initialState, action: ActionsType): Array<TodolistType> => {
-    switch (action.type) {
-        case 'АDD-TODOLIST': {
-            return [{
-                id: action.todolistId,
-                title: action.title,
-                filter: 'all'
-            }, ...state]
-        }
-        default:
-            return state;
+export const todolistsReducer = (
+  state: Array<TodolistType> = initialState,
+  action: ActionsType
+): Array<TodolistType> => {
+  switch (action.type) {
+    case addTodolistACTitle: {
+      return [
+        {
+          id: action.todolistId,
+          title: action.title,
+          filter: "all",
+        },
+        ...state,
+      ];
     }
-}
+    default:
+      return state;
+  }
+};
 
-
-export const addTodolistAC = (title: string) => ({ type: 'ADD-TODOLIST', title: title, todolistId: v1()})
-
+export const addTodolistAC = (title: string) => ({
+  type: addTodolistACTitle,
+  title: title,
+  todolistId: v1(),
+});
